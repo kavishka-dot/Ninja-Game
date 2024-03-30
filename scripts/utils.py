c130e7f2b693dccb7883cd2951896494d4229446
Kavishka Abeywardana <82282978+kavishka-dot@users.noreply.github.com>
2024-03-31 05:29:49 +0530

    Add files via upload

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,28 @@ def load_images(path):
         images.append(load_image(path + '/' + img_name))
 
     return images
+
+class Animation:
+    def __init__(self, images, img_dur = 5, loop = True):
+        self.images = images
+        self.img_duration = img_dur
+        self.loop = loop
+        self.done = False
+        self.frame = 0
+
+    def copy(self):
+        return Animation(self.images, self.img_duration, self.loop) # creates a copy. However, points to the same set of variables
+    
+
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame + 1 >= self.img_duration * len(self.images) - 1:
+                self.done = True
+    
+    def img(self):
+        return self.images[int(self.frame/ self.img_duration)]
+    
